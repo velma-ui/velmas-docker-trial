035f0283b583ca2ce6b8596add07fcae26efd23b
Alma Velma <2208995@students.kca.ac.ke>
2025-12-19 03:33:31 -0500

    Updated app.py and added docker-compose.yml

--- app.py
+++ app.py
@@ -4,7 +4,8 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hey, this is Velma’s Docker trial application."
+    return "Hey, this is Velma’s Docker trial application docker compose included!"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=3000)
+
